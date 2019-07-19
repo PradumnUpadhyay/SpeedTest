b96826353a19c766ed1b2aa839ce912d694c625c
PradumnUpadhyay <yagami_light@DESKTOP-27G8GEK.localdomain>
2019-07-19 21:00:51 +0530

    Speed Test

---
+++ index.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+var speedTest    =  require('speedtest-net');
+var ProgressBar  =  require('progress');
+var chalk        =  require("chalk");
+
+var test = speedTest({maxTime:5000});
+
+var bar,pingTime;
+
+// Event triggered when best Server is found.
+
+test.on('testserver', function(server) {
+    pingTime = server.bestPing;
+});
+
+// Invoke every time when download speed check begin
+// Return completion value in percentage
+
+test.on('downloadprogress', function(val){
+    prog('Checking Download Speed ', val);
+});
+
+// Invoke every time when upload speed check begin
+// Return completion value in percentage
+
+test.on('uploadprogress', function(val){
+    prog('Checking Upload Speed ', val);
+});
+
+// Invoke When final data recieved
+
+test.on('data',function(data){
+    console.log(chalk.cyan("Ping : "), Math.abs(pingTime),chalk.dim('ms'));
+    console.log(chalk.cyan("Download Speed : ") + data.speeds.download + chalk.dim(" MBps"));
+    console.log(chalk.cyan("Upload Speed : ") + data.speeds.upload + chalk.dim(" MBps"));
+});
+
+// in case of error, exit.
+
+test.on('error',function(error){
+    process.exit(1);
+});
+
+function prog(info, val){
+    // if its completed, terminate current progress.
+    if (val>=100){
+         if (bar) bar.terminate();
+        bar=null;
+        console.log(bar);
+        return;
+    }
+    // if bar object is not created
+      if (!bar) {
+        // var green = 'u001b[42m u001b[0m',
+        //       red = 'u001b[41m u001b[0m';
+        bar = new ProgressBar(' '+info+' [:bar] :percent', {
+            complete: '=',
+            incomplete: ' ',
+            clear: true,
+            width:40,
+            total: 100
+        });
+    }
+    
+    // else update the bar with coming value.
+    bar.update(val/100);
+}
